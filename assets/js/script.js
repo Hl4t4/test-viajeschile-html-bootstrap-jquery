1de4b12e6a0e4e0dad3f98933c0f09b79c3ec0f6
Jose Latapiatt <jose.latapiatt@proton.me>
2024-02-16 19:53:59 -0300

    Agregado alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,29 @@ var cardImage2 = document.getElementById("card-image-2");
 var cardImage3 = document.getElementById("card-image-3");
 var cardImage4 = document.getElementById("card-image-4");
 
+// Variable para lugar de premio de alerta
+var prizeNumber = 10000;
 
+const alertPlaceholder = document.getElementById('surprisePrize')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Felicidades has sido la persona #' + prizeNumber + ' en presionar nuestro logo. por un monto total de $' + (Math.random()*10000).toFixed(0), 'success')
+    prizeNumber++;
+  })
+}
 
 $(document).ready(function(){
     // Una vez cargada la página ajusta que todas las imagenes en los cards tengan el mismo alto
