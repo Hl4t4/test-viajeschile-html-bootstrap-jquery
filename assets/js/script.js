345abf5336ec02405055967f70e931bfda219de4
Jose Latapiatt <jose.latapiatt@proton.me>
2024-02-16 17:26:14 -0300

    Seccion destacados

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// En caso de usar tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+var cardImage1 = document.getElementById("card-image-1");
+var cardImage2 = document.getElementById("card-image-2");
+var cardImage3 = document.getElementById("card-image-3");
+var cardImage4 = document.getElementById("card-image-4");
+
+
+
+$(document).ready(function(){
+    cardImage2.style.maxHeight = cardImage1.clientHeight + 'px';
+    cardImage3.style.maxHeight = cardImage1.clientHeight + 'px';
+    cardImage4.style.maxHeight = cardImage1.clientHeight + 'px';
+
+    $(window).on('resize', function() {
+        cardImage2.style.maxHeight = cardImage1.clientHeight + 'px';
+        cardImage3.style.maxHeight = cardImage1.clientHeight + 'px';
+        cardImage4.style.maxHeight = cardImage1.clientHeight + 'px';
+    });
+});
