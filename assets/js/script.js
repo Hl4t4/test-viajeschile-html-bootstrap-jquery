6815b1bb3ce35531989962d4c6f15986474bee62
Jose Latapiatt <jose.latapiatt@proton.me>
2024-02-16 20:14:41 -0300

    hotfix#3 tamaño card images

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,9 @@ if (alertTrigger) {
 $(document).ready(function(){
     // Una vez cargada la página ajusta que todas las imagenes en los cards tengan el mismo alto
     $('#card-image-1').ready(function() {
+        console.log('This')
         cardImage1 = document.getElementById("card-image-1");
+        console.log(cardImage1.clientHeight)
         cardImage2.style.maxHeight = cardImage1.clientHeight + 'px';
         cardImage3.style.maxHeight = cardImage1.clientHeight + 'px';
         cardImage4.style.maxHeight = cardImage1.clientHeight + 'px';
