0ebf70debabd7073017aac622fadf4fceef5b34b
Jose Latapiatt <jose.latapiatt@proton.me>
2024-02-16 19:24:18 -0300

    Agregados comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+// Binding de cards de imagenes
 var cardImage1 = document.getElementById("card-image-1");
 var cardImage2 = document.getElementById("card-image-2");
 var cardImage3 = document.getElementById("card-image-3");
@@ -10,16 +11,19 @@ var cardImage4 = document.getElementById("card-image-4");
 
 
 $(document).ready(function(){
+    // Una vez cargada la página ajusta que todas las imagenes en los cards tengan el mismo alto
     cardImage2.style.maxHeight = cardImage1.clientHeight + 'px';
     cardImage3.style.maxHeight = cardImage1.clientHeight + 'px';
     cardImage4.style.maxHeight = cardImage1.clientHeight + 'px';
 
+    // Ajuste de alto de los cards con imagenes cuando se ajusta el tamaño de la página
     $(window).on('resize', function() {
         cardImage2.style.maxHeight = cardImage1.clientHeight + 'px';
         cardImage3.style.maxHeight = cardImage1.clientHeight + 'px';
         cardImage4.style.maxHeight = cardImage1.clientHeight + 'px';
     });
 
+    // Funcion de ayuda para agregar un smooth scroll mas lento que el de css por defecto
     function scrollToAnchor(aid){
         const destination = $(aid);
           $('html,body').animate({
@@ -27,6 +31,7 @@ $(document).ready(function(){
           },'slow');
       }
       
+      // Listener usando jquery para escuchar los clicks en la navbar para moverse dentro de la página
       $(document).on('click', '.nav-link', function(){
         scrollToAnchor($(this)[0].hash);
       })
